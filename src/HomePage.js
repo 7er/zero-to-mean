cf6a52e7f8ee5a6036b3da751699b3216bf7e76d
Syver Enstad <syver.enstad@gmail.com>
2020-03-11 12:45:00 +0100

    Following zero to mean tutorial

---
+++ src/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const HomePage = props => {
+  return (
+    <div>
+      <h1>Welcome to the Event Manager</h1>
+    </div>
+  );
+};
+
+HomePage.propTypes = {
+
+};
+
+export default HomePage;
